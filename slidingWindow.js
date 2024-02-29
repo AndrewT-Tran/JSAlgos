b0172491e150e8793aec20030b3c1a25fb0266c1
Andrew T <me@andrewtran-shi.dev>
2024-02-29 12:09:20 -0800

    new file:   slidingWindow.js

---
+++ slidingWindow.js
@@ -0,0 +1,46 @@
+function findMaxSumOfSequence(listOfItems, sequenceLength) {
+    if (listOfItems.length < sequenceLength) {
+        return null;
+    }
+    // creates the window
+    let start = 0,
+        end = 0,
+        maxSum = 0,
+        windowSum = 0;
+    // creates the initial window
+    while (end < sequenceLength) {
+        windowSum += listOfItems[end];
+        end++;
+        maxSum = windowSum;
+        //creates the sum of first window
+    }
+    // we will subtract and add the elements to the window to get the sum of the next window
+    while (start + sequenceLength < listOfItems.length) {
+        windowSum = windowSum - listOfItems[start] + listOfItems[end];
+        // we are subtracting the previous number and adding the next number and updating the windowSum
+        console.log(windowSum)
+        console.log('listOfItems[start]', listOfItems[start])
+        console.log('listOfItems[end]', listOfItems[end])
+        // we are updating the maxSum if the windowSum is greater than maxSum
+        maxSum = Math.max(windowSum, maxSum);
+        // we are moving the window by incrementing the start and end
+        start++;
+        end++;
+    }
+
+    console.log("maxSum", maxSum, "windowSum", windowSum, "end", end, "start", start)
+    return maxSum;
+}
+
+
+// const input = [1, 2, 6, 2, 4, 1],
+
+const input = [1, 2, 6, 2, 4, 1, 4, 2, 5, 8],
+    windowSize = 3;
+
+console.log(
+    `Maximum sum of a sub-array of window size ${windowSize} is ${findMaxSumOfSequence(
+        input,
+        windowSize
+    )}`
+);
