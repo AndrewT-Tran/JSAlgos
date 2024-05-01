8bb935c02298f74e3867f50f08ab3361d76f117d
Andrew T <me@andrewtran-shi.dev>
2024-05-01 09:15:03 -0700

    updated

---
+++ bigFlipper2.js
@@ -0,0 +1,19 @@
+Syvc rirel puhax bs a punenpgref va n fgevat, jurer a vf nal cbfvgvir vagrtre terngre guna 1.
+
+Abgr gung guvf vf vagragvbanyyl irel fvzvyne gb gur cerivbhf ceboyrz.
+
+Cyrnfr sbphf ba trggvat n jbexvat fbyhgvba jvgu gur gbbyf lbh xabj jryy.
+
+Cenpgvpr gur vagrenpgvir/pbyynobengvir vagreivrj fglyr gung'f qrfpevorq va gur qbphzragngvba.
+
+Rknzcyr:
+ine vachg = 'n fubeg rknzcyr';
+ine bhgchg = syvcRirelAPunef(vachg, 5);
+pbafbyr.ybt(bhgchg); // --> buf nkr geryczn
+
+Oernxvat guvf rknzcyr qbja cvrpr ol cvrpr:
+'n fub' -> 'buf n'
+'eg rk' -> 'kr ge'
+'nzcyr' -> 'ryczn'
+
+-> 'buf nkr geryczn'
